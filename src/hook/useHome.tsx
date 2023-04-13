96541a21d8658b8564aba5d4cb1fd6e9539b2214
Paulo Augusto <paulo-augusto12@outlook.com>
2023-04-13 13:18:22 -0300

    feat: using api route call

--- src/hook/useHome.tsx
+++ src/hook/useHome.tsx
@@ -3,13 +3,17 @@ export function useHome() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleSubmitLogin(email: string, password: string) {
+  async function handleSubmitLogin(email: string, password: string) {
     const params = {
       login: email,
       password,
     };
+    const response = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify(params),
+    });
 
-    console.log("Login efetuado: ", JSON.stringify(params));
+    console.log("Login efetuado: ", response);
   }
   return {
     email,
