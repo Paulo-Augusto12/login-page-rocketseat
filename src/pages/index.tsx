696ef8ad0abc5b900d5c337658abbbb25a1c00fd
Paulo Augusto <paulo-augusto12@outlook.com>
2023-04-13 10:35:43 -0300

    feat: added rocketseat logo asset

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
-import { Text } from "@chakra-ui/react"
+import { Flex, Grid, Text } from "@chakra-ui/react";
 export default function Home() {
   return (
-      <Text color={'purple.500'} fontSize={36}>Hello world</Text>
-  )
+    <Grid
+      as={"main"}
+      height={"100vh"}
+      templateColumns={"1fr 480px 480px 1fr"}
+      templateRows={"1fr 480 1fr"}
+      templateAreas={" '. . . .' '. logo form .' ' . . . .' "}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Flex gridArea={"logo"} flexDir={"column"} alignItems={"flex-start"}>
+        <img src="/rocketseat.svg" alt="rocketseat" />
+      </Flex>
+      <Text color={'purple.500'}>
+        aaa
+      </Text>
+    </Grid>
+  );
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,53 +1,43 @@
 import { extendTheme, theme, type ThemeConfig } from "@chakra-ui/react";
 
-const colors = {
-  ...theme.colors,
-  purple: {
-    ...theme.colors.purple,
-    500: "#8257e5",
+const customTheme = {
+  colors: {
+    purple: {
+      ...theme.colors.purple,
+      500: "#8257e5",
+    },
+    gray: {
+      ...theme.colors.gray,
+      300: "#e1e1e6",
+      600: "#29292e",
+      700: "#202024",
+      800: "#121214",
+    },
   },
-  gray: {
-    ...theme.colors.gray,
-    300: "#e1e1e6",
-    600: "#29292e",
-    700: "#202024",
-    800: "#121214",
+  fontSizes: {
+    ...theme.fontSizes,
+    "6xl": "54px",
   },
-};
-
-const fonts = {
-  body: "Roboto, system-ui, sans-serif",
-  heading: "Roboto, system-ui, sans-serif",
-  mono: "Menlo, monospace",
-};
-
-const fontWeights = {
-  normal: 400,
-  medium: 500,
-  bold: 700,
-};
-
-const radii = {
-  sm: "5px",
-  md: "8px",
-};
-
-const fontSizes = {
-  ...theme.fontSizes,
-  "6xl": "54px",
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    bold: 700,
+  },
+  radii: {
+    sm: "5px",
+    md: "8px",
+  },
+  fonts: {
+    body: "Roboto, system-ui, sans-serif",
+    heading: "Roboto, system-ui, sans-serif",
+    mono: "Menlo, monospace",
+  },
 };
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };
-const useTheme = extendTheme({
-  colors,
-  config,
-  fonts,
-  fontWeights,
-  radii,
-  fontSizes,
-});
+const useTheme = extendTheme(customTheme, { config });
 
 export default useTheme;
