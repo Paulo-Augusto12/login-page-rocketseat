78eeb6bdce186b0e07f1c4263fcffd90c44c945c
Paulo Augusto <paulo-augusto12@outlook.com>
2023-04-13 13:26:12 -0300

    feat: calling routeUseCase function

--- src/hook/useHome.tsx
+++ src/hook/useHome.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
+import { login } from "../../apiUseCases/login";
 export function useHome() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   async function handleSubmitLogin(email: string, password: string) {
-    const params = {
-      login: email,
-      password,
-    };
-    const response = await fetch("/api/login", {
-      method: "POST",
-      body: JSON.stringify(params),
-    });
-
-    console.log("Login efetuado: ", response);
+    if (!email.trim()) {
+      return;
+    }
+    if (!password.trim()) {
+      return;
+    }
+    login(email, password);
+    setEmail("");
+    setPassword("");
   }
+
   return {
     email,
     setEmail,
